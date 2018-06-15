a75c691faa615319c45469efd313df6e58fb812e
mlaskowski17 <marcin.laskowski17@imperial.ac.uk>
2018-06-15 19:37:35 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Autoencoders
- Implementation of differnet types of autoencoders in PyTorch
+
+ Implementation of different types of autoencoders in PyTorch
+
+
+### Introduction
+Neural Networks are remarkably efficient tools to solve a number of really difficult problems. The first application of neural networks usually revolved around classification problems. Classification means that we have an image as an input and the output is for example simple decision whether it is a car or a chair. The input will have as many nodes as there are pixels in the input image, and the output will have to units, so we look at this two to find which fires the most to make the decision. Between this two there are hidden layers where the neural network is asked to build an inner representation of the problem that is efficient at recognizing these objects.
+
+So the question is what is autoencoder?
+An autoencoder is an interesting variant with two important changes:
+- First, the number of neurons is the same in the input and the output, therefore we can expect that the output is an image that is not only the same size as the input but actually is the same image.
+
+<p align="center">
+  <img width="800" src="https://github.com/mlaskowski17/Autoencoders/blob/master/images/first.jpg">
+</p>
+
+- Second, we have a bottleneck (latent space) in one of these layers. This means that the number of neurons in that layer is much less than we would normally see, therefore it has to find the way to represent this kind of data the best it can with a much smaller number of neurons.
+
+<p align="center">
+  <img width="800" src="https://github.com/mlaskowski17/Autoencoders/blob/master/images/second.png">
+</p>
+
+To sum up:
+**Autoencoders are neural networks that are capable of creating sparse representations of the input data and can therefore be used for image compression.** There are denoising autoencoders that after learning these sparse representations, can be presented with noisy images. What is even better is a variant that is called the variational autoencoder that not only learns these sparse representations, but can also draw new images as well. We can, for instance, ask it to create new handwritten digits and we can actually expect the results to make sense.
---
+++ Simple_autoencoder/autoencoder_MNIST.py
@@ -0,0 +1,131 @@
+'''
+Implementation of the simple Autoencoder in PyTorch. Dataset used to perform this task is MNIST.
+The input is binarized and Binary Cross Entropy has been used as the loss function.
+The hidden layer contains 64 units.
+Reconstructions are saved each 10th epoch.
+'''
+
+import os
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+
+
+###################################################################################################
+# Define Hyperparameters
+num_epochs = 200
+batch_size = 128
+learning_rate = 1e-3
+
+
+###################################################################################################
+# FUNCTIONS - DATA OPERATIONS
+
+# create directory 'auto_output' in which input and output will be saved
+if not os.path.exists('./auto_output'):
+    os.mkdir('./auto_output')
+
+
+# function to visualise the image
+def to_img(x):
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+
+# function to plot sample image
+def plot_sample_img(img, name):
+    img = img.view(1, 28, 28)
+    save_image(img, './sample_{}.png'.format(name))
+
+
+# normalization where the entire range of values of X from min to max are mapped to the range 0 to 1
+def min_max_normalization(tensor, min_value, max_value):
+    min_tensor = tensor.min()
+    tensor = (tensor - min_tensor)
+    max_tensor = tensor.max()
+    tensor = tensor / max_tensor
+    tensor = tensor * (max_value - min_value) + min_value
+    return tensor
+
+
+# Rounds the values of a tensor to the nearest integer, element-wise.
+def tensor_round(tensor):
+    return torch.round(tensor)
+
+
+###################################################################################################
+# INPUT DATA
+
+# create Transforms which will perform operations on the dataset. All operations in one varible
+img_transform = transforms.Compose([
+    transforms.ToTensor(),  # transform input images to Tensor
+    transforms.Lambda(lambda tensor:min_max_normalization(tensor, 0, 1)),  # normalization
+    transforms.Lambda(lambda tensor:tensor_round(tensor))  # round the values of tensor
+])
+
+# download MNIST dataset if not downloaded
+dataset = MNIST('./data', transform=img_transform, download=True)
+# load previously downloaded data in batches and shuffle
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+###################################################################################################
+# NEURAL NETWORK
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 256),
+            nn.ReLU(True),
+            nn.Linear(256, 64),
+            nn.ReLU(True))
+        self.decoder = nn.Sequential(
+            nn.Linear(64, 256),
+            nn.ReLU(True),
+            nn.Linear(256, 28 * 28),
+            nn.Sigmoid())
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+model = autoencoder().cuda()
+# definition of the LOSS FUNCTION: Binary Cross Entropy
+criterion = nn.BCELoss()
+# type of OPTIMIZER: Adam
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+
+###################################################################################################
+# TRAINING
+
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data
+        img = img.view(img.size(0), -1)
+        img = Variable(img).cuda()
+        # ===================forward=====================
+        output = model(img)
+        loss = criterion(output, img)
+        MSE_loss = nn.MSELoss()(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    print('epoch [{}/{}], loss:{:.4f}, MSE_loss:{:.4f}'
+          .format(epoch + 1, num_epochs, loss.data[0], MSE_loss.data[0]))
+    if epoch % 10 == 0:
+        x = to_img(img.cpu().data)
+        x_hat = to_img(output.cpu().data)
+        save_image(x, './auto_output/x_{}.png'.format(epoch))
+        save_image(x_hat, './auto_output/x_hat_{}.png'.format(epoch))
+
+torch.save(model.state_dict(), './sim_autoencoder.pth')
